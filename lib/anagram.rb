1234b3df913d46bca2768b1c082bac5105c00128
Michelle Ranly <meranly8@gmail.com>
2020-10-01 03:03:03 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -5,10 +5,16 @@ class Anagram
     @word = word
   end
   
+<<<<<<< HEAD
   def match(words)
     words.select do |w|
       w.split("").sort == word.split("").sort
     end
   end
 
+=======
+  def match
+    
+  end
+>>>>>>> b14151771ec02dac5a8eab56b8173b47a28bdd89
 end
